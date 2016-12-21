1aac1c033f590e667457b37bf93e230e91fce7fa
Adam Tse <mr.adam.tse@gmail.com>
2016-12-21 18:18:55 -0500

    proposal-list temp data

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal( 15 , 'Test Compnary', 'http://www.itsadamtse.com', 'Ruby on Rails', 150, 210, 20, 'unknown91sadsadas1@gmail.com')
+  proposalTwo: Proposal = new Proposal( 95 , 'Test Compnary', 'http://www.itsadamtse.com', 'Ruby on Rails', 150, 210, 20, 'unknown91sadsadas1@gmail.com')
+  proposalThree: Proposal = new Proposal( 115 , 'Test Compnary', 'http://www.itsadamtse.com', 'Ruby on Rails', 150, 210, 20, 'unknown91sadsadas1@gmail.com')
+
+  proposals: Proposal[] = [this.proposalOne, this.proposalTwo, this.proposalThree]
+}
