0428026f68944cce8284b44ee797edb4c500bd88
Adam Tse <mr.adam.tse@gmail.com>
2016-12-20 21:44:58 -0500

    quick component config helloworldstyle

---
+++ app/homepage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-app',
+  template: `
+  <h1> Hello this is my home page</h1>
+  `
+})
+export class HomepageComponent {}
