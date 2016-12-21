00d0f3da84db0a6c39c4ea48888b01e00706d070
Adam Tse <mr.adam.tse@gmail.com>
2016-12-21 17:46:35 -0500

    documents added + test docs

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
 moduleId: module.id,
 selector: 'documents',
 templateUrl: 'documents.component.html'
 })
-export class DocumentsComponent {}
+export class DocumentsComponent {
+  documents: Document[] = [
+    {
+      title: "This is a Test",
+      description: "Test data",
+      file_url: "www.google.com",
+      updated_at: "01/01/1010",
+      image_url: "www.google.com"
+    },
+    {
+      title: "DOc 2",
+      description: "Test data",
+      file_url: "www.google.com",
+      updated_at: "01/01/1010",
+      image_url: "www.google.com"
+    },
+    {
+      title: "Test Doc 3",
+      description: "Test data",
+      file_url: "www.google.com",
+      updated_at: "01/01/1010",
+      image_url: "www.google.com"
+    }
+  ]
+
+}
