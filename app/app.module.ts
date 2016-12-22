92d7984f4889247c3e3e73c68600f4bd5b52ff4c
Adam Tse <mr.adam.tse@gmail.com>
2016-12-21 20:09:40 -0500

    added proposal structure before form

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,12 +5,13 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AppComponent } from './app.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [ BrowserModule, AppRoutingModule ],
-  declarations: [ AppComponent, HomepageComponent, DocumentsComponent, ProposalListComponent ],
+  declarations: [ AppComponent, HomepageComponent, DocumentsComponent, ProposalListComponent, ProposalNewComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
